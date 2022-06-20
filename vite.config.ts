8f0a489ac2782f20ad0429a9ac8112ab85993968
Sebastian-s14 <sebastian.sanchez@tecsup.edu.pe>
2022-06-20 00:20:45 -0500

    fix: base vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sebastian-s14/gifs-app/',
+  base: '/gifs-app/',
 })
